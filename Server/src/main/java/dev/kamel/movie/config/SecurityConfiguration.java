22daf10740b1e0efdfe452e12b34ceb507579863
kamel996 <kamelnassar2828@gmail.com>
2024-03-20 14:41:54 +0200

    resolved cors error

---
+++ Server/src/main/java/dev/kamel/movie/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package dev.kamel.movie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            public void addCorsMappings(final CorsRegistry registry){
+                registry.addMapping("/**").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
+}
